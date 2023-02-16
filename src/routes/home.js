81a22c27702d680f5dedee8c757c83ba84f9498d
jonathan lizama <jona.lizama06@gmail.com>
2023-02-15 22:20:46 -0300

    se realiza modularización de documentación

--- src/routes/home.js
+++ src/routes/home.js
@@ -1,16 +1,16 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 const routesIndex = (req, res) => {
-    //create html page
-    res.send(`
+  //create html page
+  res.send(`
         <h1>Routes</h1>
         <ul>
             <li><a href="/establecimientos">Establecimientos</a></li>
 <!--            <li><a href="/coordenadas">Coordenadas</a></li>-->
         </ul>
     `);
-}
+};
 
-router.get('/', routesIndex);
+router.get("/", routesIndex);
 
 module.exports = router;
